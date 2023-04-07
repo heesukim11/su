c0cce6aa9c3b2378a02c9afac7dc16cb08bf922e
su <jammimo@naver.com>
2023-04-07 14:53:21 +0900

    포트폴리오

---
+++ 클론코딩/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    /* swiper js */
+    var swiper = new Swiper(".mainBanner", {
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+        });
+        
+
+
+});
